1bc32c7bcf11b740ec96ef01934e31aa1d26acc3
David Rios <davidsamuelrios07@gmail.com>
2020-06-27 21:49:59 -0400

    Added colors configurations for music player

---
+++ lib/src/ui/app_colors.dart
@@ -0,0 +1,29 @@
+// Music Player
+import 'package:flutter/material.dart';
+
+// Music Player Colors
+const Color backgroundColor = Color(0xff0D0D0D);
+
+// Cassette Colors
+const Color cassetteColor = Color(0xff655E70);
+const Color darkStripColor = Color(0xff753922);
+const Color stripColor = Color(0xff874223);
+const Color gearColor = Color(0xffB2AFAF);
+const Color gearBorderColor = Color(0xff707070);
+const Color gearExternalColor = Colors.black;
+const Color letterBackground = Color(0xffECE2CE);
+const Color darkBorderShadow = Color(0xff202323);
+const Color lightBorderShadow = Color(0xff173243);
+const Color baseColor = Color(0xff616868);
+const Color holeColor = Color(0xff47494F);
+const Color cassetteLines = Color(0xff3D3F3F);
+
+// Screw Colors
+const Color screwColor = Color(0xff9397A5);
+const Color screwShadow = Color(0xff7B8190);
+
+// Sticker Colors
+const Color yellowSticker = Color(0xffF2BC1B);
+
+// Player Buttons
+const Color playerButtonShadow = Color(0xff7B8190);
